d3a3161145e97626f6289434f271083a04620e54
Ebenezer Sambieni <sambieniebenezer1@gmail.com>
2026-06-01 17:11:15 +0200

    premier commit

--- netlify/prepare-public.js
+++ netlify/prepare-public.js
@@ -3,6 +3,7 @@ const path = require('path');
 
 const rootDir = path.resolve(__dirname, '..');
 const distDir = path.join(rootDir, 'dist');
+const fontAwesomeDir = path.join(rootDir, 'node_modules', '@fortawesome', 'fontawesome-free');
 
 async function copyPublicFiles() {
   await fs.rm(distDir, { recursive: true, force: true });
@@ -10,6 +11,17 @@ async function copyPublicFiles() {
 
   await fs.copyFile(path.join(rootDir, 'index.html'), path.join(distDir, 'index.html'));
   await fs.cp(path.join(rootDir, 'front-end'), path.join(distDir, 'front-end'), { recursive: true });
+
+  await fs.cp(
+    path.join(fontAwesomeDir, 'css'),
+    path.join(distDir, 'assets', 'fontawesome', 'css'),
+    { recursive: true }
+  );
+  await fs.cp(
+    path.join(fontAwesomeDir, 'webfonts'),
+    path.join(distDir, 'assets', 'fontawesome', 'webfonts'),
+    { recursive: true }
+  );
 }
 
 copyPublicFiles().catch((error) => {
